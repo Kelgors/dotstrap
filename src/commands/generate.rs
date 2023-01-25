bc325f4a52880624b99f6636ec1b7848376f6097
Kelgors <matthieuboheas@pm.me>
2023-01-25 18:53:45 +0100

    call run_generate

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -3,25 +3,21 @@ use pathbuf::pathbuf;
 
 use crate::{
     action::{
-        compact_mergeable_actions, transform_package_to_actions, SystemAction, generation::generate_shell_script,
+        compact_mergeable_actions, generation::generate_shell_script, transform_package_to_actions,
+        SystemAction,
     },
     host::HostDefinition,
     lockfile::build_action_diff,
     resolver,
 };
 
-pub struct RunGenerateOptions {
-    pub dry: bool,
-}
+pub struct RunGenerateOptions {}
 
-pub fn run_generate(hostname: String, options: RunGenerateOptions) -> Result<()> {
+pub fn run_generate(hostname: String, _: RunGenerateOptions) -> Result<()> {
     let host_definition = HostDefinition::from_path(&pathbuf!["hosts", &hostname])?;
     let packages_repo = resolver::resolve_dependencies(&host_definition.package)?;
-    let next_system_actions = transform_package_to_actions(
-        &host_definition.package,
-        &packages_repo,
-        &mut vec![],
-    )?;
+    let next_system_actions =
+        transform_package_to_actions(&host_definition.package, &packages_repo, &mut vec![])?;
     // merge next actions with cleaning actions
     let all_actions: Vec<SystemAction> = build_action_diff(&next_system_actions)?;
     // compacting actions when possible
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
 use clap::Parser;
 use commands::add::{run_add, RunAddOptions};
+use commands::generate::{run_generate, RunGenerateOptions};
 use commands::init::{run_init, RunInitOptions};
 use commands::install::{run_install, RunInstallOptions};
 use commands::remove::{run_remove, RunRemoveOptions};
-use pathbuf::pathbuf;
-use std::env;
 use std::fs;
 
 mod action;
@@ -17,13 +16,6 @@ mod lockfile;
 mod package;
 mod resolver;
 
-use crate::action::compact_mergeable_actions;
-use crate::action::generation::generate_shell_script;
-use crate::action::transform_package_to_actions;
-use crate::action::SystemAction;
-use crate::host::HostDefinition;
-use crate::lockfile::build_action_diff;
-
 fn main() -> Result<()> {
     let args = cli::Args::parse();
     let machine_hostname = String::from(
@@ -31,7 +23,6 @@ fn main() -> Result<()> {
             .unwrap_or("".to_string())
             .trim(),
     );
-    let pwd = env::current_dir()?;
 
     match args.action {
         Some(cli::Action::Init {}) => {
@@ -43,12 +34,15 @@ fn main() -> Result<()> {
             commit,
             push,
         }) => {
-            run_add(machine_hostname, RunAddOptions {
-                package_names,
-                install,
-                commit,
-                push
-            })?;
+            run_add(
+                machine_hostname,
+                RunAddOptions {
+                    package_names,
+                    install,
+                    commit,
+                    push,
+                },
+            )?;
         }
         Some(cli::Action::Remove {
             package_names,
@@ -56,22 +50,24 @@ fn main() -> Result<()> {
             commit,
             push,
         }) => {
-            run_remove(machine_hostname, RunRemoveOptions {
-                package_names,
-                install,
-                commit,
-                push
-            })?;
+            run_remove(
+                machine_hostname,
+                RunRemoveOptions {
+                    package_names,
+                    install,
+                    commit,
+                    push,
+                },
+            )?;
         }
         Some(cli::Action::Generate { hostname }) => {
             let hostname = hostname.unwrap_or(machine_hostname);
             // Load host definition and prepare system actions from it
-            
+            run_generate(hostname, RunGenerateOptions {})?;
         }
         Some(cli::Action::Install { hostname, dry }) => {
             let hostname = hostname.unwrap_or(machine_hostname);
-            let options = RunInstallOptions { dry };
-            run_install(hostname, options)?;
+            run_install(hostname, RunInstallOptions { dry })?;
         }
         None => {}
     }
